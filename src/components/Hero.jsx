2189909154d4bf51f0c105be91661b8faf018674
“rahmamahdi02” <rahmamahdi.02@gmai.com>
2025-05-12 03:01:13 -0700

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,21 @@
 import heroBg from '../assets/heros-bg.png';
+import heroText from '../assets/hero-text.png';
 
 export default function Hero() {
   return (
     <section id="hero" className="relative">
-        <img className='absolute inset-0 w-full h-full object-cover -z-[2]' src={heroBg} alt="" />
-        <div className="bg-black/15 absolute inset-0 -z-[1]"></div>
-        <div className="wrapper flex items-center min-h-screen text-white ">
-            <div className="max-w-[400px] sm:max-w-[550px] sm:pt-[150px] sm:pb-[60px]">
-                <h1>Are you <span className='text-primaryColor font-bold'>ready to</span> thrive in your role as a mother</h1>
-                <p className="my-6">Create more joy and confidence while guiding your children with love, kindess, and firm limits.</p>
-                <div>
-                    <a href="https://calendly.com/amanaparentcoaching/15min?month=2025-05" className="btn btn-primary">Book Your Clarity Call Today</a>
-                </div>
-            </div>   
+      <img className="absolute inset-0 w-full h-full object-cover -z-[2]" src={heroBg} alt="" />
+      <div className="bg-black/15 absolute inset-0 -z-[1]"></div>
+
+      <div className="wrapper flex items-center min-h-screen text-white">
+        <div className="pt-[150px] pb-[60px] -ml-4 sm:-ml-6">
+          <img
+            src={heroText}
+            alt="Hero Text"
+            className="w-[90vw] max-w-[800px] h-auto"
+          />
         </div>
+      </div>
     </section>
-  )
+  );
 }
