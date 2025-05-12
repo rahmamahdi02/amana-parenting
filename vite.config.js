3da9cf61e825bfbdab675dc57bd266827701aa7a
“rahmamahdi02” <rahmamahdi.02@gmai.com>
2025-05-12 11:20:08 -0700

    error with build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/amana-parenting/',
   css: {
     postcss: {
       plugins: [tailwindcss()],
