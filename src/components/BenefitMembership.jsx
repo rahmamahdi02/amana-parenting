ff5fd5d655a69e4cf7fcd165345c76a4d7cba0dc
“rahmamahdi02” <rahmamahdi.02@gmai.com>
2025-05-25 03:04:22 -0700

    feat: update text sizing

--- src/components/BenefitMembership.jsx
+++ src/components/BenefitMembership.jsx
@@ -10,7 +10,9 @@ export default function BenefitMembership() {
         <SectionTitle>
           <h2><span className="text-primaryColor font-bold">Here are the 4 Pillars,
           </span> we build together:</h2>
-          <p>Join a nurturing, Sunnah-based parenting community designed to support Muslim moms in building peaceful, purposeful homes.</p>
+          <p className="text-xl mb-6">Join a nurturing, Sunnah-based parenting community designed to support Muslim moms in building peaceful, purposeful homes.
+            </p>
+          <p>At The Confident Mamas Mentorship, I help moms like you step into their God-given leadership role—trusting Allah, trusting themselves, and raising the next generation with strong hearts, sound minds, and sincere faith</p>
         </SectionTitle>
         <div className="grid sm:grid-cols-2 lg:grid-cols-2 gap-4">
           {benefitData.map((item, index) => (
