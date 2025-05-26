cadfbce086e5df97ea23b89832dca7b13432ff61
“rahmamahdi02” <rahmamahdi.02@gmai.com>
2025-05-26 14:00:36 -0700

    save features on branch

--- src/Membership.jsx
+++ src/Membership.jsx
@@ -21,8 +21,8 @@ function Membership() {
     <main>
         <HeroMembership />
         <About />
-        <CtaMembership />
         <Counter />
+        <CtaMembership />
         <Treatmants />
         <BenefitMembership />
         <Testimonials />
